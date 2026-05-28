b135488c784afc318bdf161b4b0d360957467538
gledyson007 <fgledyson5@gmail.com>
2026-05-28 14:50:18 -0300

    Corrige CSS do Admin e adiciona criacao automatica de usuario

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import ConfigurracaoSite, Servico
 
 def index(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
     configuracao = ConfigurracaoSite.objects.first()
 
     servicos = Servico.objects.filter(ativo=True)
